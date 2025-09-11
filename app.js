bbd8b130e7ab5c51b6552e69db93be4bb0627e94
sabarees48 <54196405+sabarees48@users.noreply.github.com>
2025-09-11 23:03:51 +0530

    App1 commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+res.send('Hello from Node.js app on EKS from APP1!');
+});
+app.listen(port, () => {
+console.log(`App running at http://localhost:${port}`);
+});
